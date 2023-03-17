8517617d9596021cb5dac11bc47e8da1f5b58d55
UGPS <122919964+UGPS-Admin@users.noreply.github.com>
2023-03-16 20:05:41 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+  var replaceWords = {
+    "original word 1": "replacement word 1",
+    "original word 2": "replacement word 2",
+    // Add as many word replacements as you need
+  };
+
+  var replaceNodeText = function(node) {
+    if (node.nodeType === Node.TEXT_NODE) {
+      var text = node.textContent;
+      for (var originalWord in replaceWords) {
+        var replacementWord = replaceWords[originalWord];
+        var regex = new RegExp(originalWord, "gi");
+        text = text.replace(regex, replacementWord);
+      }
+      node.textContent = text;
+    } else {
+      for (var i = 0; i < node.childNodes.length; i++) {
+        replaceNodeText(node.childNodes[i]);
+      }
+    }
+  };
+
+  var nodes = document.body.querySelectorAll("*");
+  for (var i = 0; i < nodes.length; i++) {
+    replaceNodeText(nodes[i]);
+  }
+});
 
 const CLIENT_ID = 'rB9bHuU6T9SElL0D';
 
@@ -48,16 +76,6 @@ drone.on('open', error => {
     }
   });
 });
-
-const badWords = ["bad", "ugly"];
-
-function replaceBadWords(message) {
-  badWords.forEach(word => {
-    const regex = new RegExp(word, 'gi');
-    message = message.replace(regex, '#');
-  });
-  return message;
-}
 
 const room = drone.subscribe('observable-room');
 
